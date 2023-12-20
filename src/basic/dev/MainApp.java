5c455275b485d398d954c9e9148592da49ca175a
letrongkhanh <144077765+letrongkhanh@users.noreply.github.com>
2023-12-20 15:06:36 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+
+/**
+ * 
+ * 
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Phan Van";
+		String lastnName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + "" + lastnName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+
+		// Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+
+}
